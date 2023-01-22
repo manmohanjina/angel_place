c16acc2afe75db3f678cdee02d1d51f064e423b4
manmohanjina <manish63singh@gmail.com>
2023-01-22 22:49:03 +0530

    angel_place_cyclic.sh_change

--- index.js
+++ index.js
@@ -39,12 +39,7 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
